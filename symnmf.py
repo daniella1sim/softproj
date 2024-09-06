08ba1e67b674412c5b5be3f53d09590df3a26823
daniella1sim <128131945+daniella1sim@users.noreply.github.com>
2024-09-06 11:33:23 +0300

    added comments and subfunction for parsing user input

--- symnmf.py
+++ symnmf.py
@@ -4,6 +4,14 @@ import symnmf
 
 np.random.seed(1234)
 
+
+""""
+Verifies argument is an int
+@type arg: String
+@param arg: A string of input from user to verify
+@rtype: Touple(int, int)
+@returns: First int return 1 if arg is not an integer and 1 if it is, second returns the arg in integer form.
+"""
 def isInt(arg):
     try:
         return 0, int(arg)
@@ -19,6 +27,39 @@ def isInt(arg):
             return 1, 0
 
 
+"""
+Parses the data from the command line
+@type args: List
+@param args: A list of command line arguments
+@rtype: Tuple(int, string, string)
+@returns: A tuple of the parsed data containing K, goal, and filepath to data
+"""
+def parse_data(args):
+    if len(args) != 4:
+        print("Error: Incorrect number of arguments")
+        return -1, 0, 0
+    err_K, K = isInt(args[1])
+    if err_K:
+        print("Error: K must be an integer")
+        return -1, 0, 0
+    goal = args[2]
+    if goal not in ["symnmf", "sym", "ddg", "norm"]:
+        print("Error: goal must be one of symnmf, sym, ddg, norm")
+        return -1, 0, 0
+    filepath = sys.argv[3]
+    if (filepath[-4:] != ".txt"):
+        print("Error: file must be a .txt file")
+        return -1, 0, 0
+    return K, goal, filepath
+
+
+"""
+Creates a similarity matrix from the data
+@type data: numpy array
+@param data: A numpy array of data
+@rtype: numpy array
+@returns: A numpy array of the similarity matrix
+"""
 def similarity_matrix(data):
     N = data.shape[0]
     sim_matrix = np.zeros((N, N))
@@ -32,30 +73,21 @@ def similarity_matrix(data):
     return sim_matrix
 
 
+"""
+Main function that runs the program
+@rtype: int
+@returns: 0 if program runs successfully else 1
+"""
 def main():
-    if len(sys.argv) != 4:
-        print("Error: Incorrect number of arguments")
-        return
-    err_K, K = isInt(sys.argv[1])
-    if err_K:
-        print("Error: K must be an integer")
-        return
-    
-    goal = sys.argv[2]
-    if goal not in ["symnmf", "sym", "ddg", "norm"]:
-        print("Error: goal must be one of symnmf, sym, ddg, norm")
-        return
-    
-    filepath = sys.argv[3]
-    if (filepath[-4:] != ".txt"):
-        print("Error: file must be a .txt file")
-        return
-    
+    K, goal, filepath = parse_data(sys.argv)
+    if K == -1:
+        return 1
+ 
     data = np.genfromtxt(filepath, delimiter=',', dtype=float)
     N, D = data.shape
     if (K > N):
         print("Error: K must be less than the number of rows in the data")
-        return
+        return 1
 
     A = similarity_matrix(data)
     #D = np.diag(np.sum(A, axis=0))
