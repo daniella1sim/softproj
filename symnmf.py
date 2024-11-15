dbf1e02d05a6c41569296efa5dc865b9830bf6ad
daniella1sim <128131945+daniella1sim@users.noreply.github.com>
2024-11-15 21:23:28 +0200

    working

--- symnmf.py
+++ symnmf.py
@@ -1,41 +1,9 @@
 import sys
 import numpy as np
 import symnmf as sy
+from tools import isInt, error
 
-np.random.seed(1234)
-
-
-"""
-Prints an error message and exits the program.
-
-@rtype: None
-@returns: Exits the program with a status of 1.
-"""
-def error():
-    print("An Error Has Occured!")
-    exit(1)
-
-
-""""
-Verifies argument is an int
-@type arg: String
-@param arg: A string of input from user to verify
-@rtype: Touple(int, int)
-@returns: arg in integer form.
-"""
-def isInt(arg):
-    try:
-        return int(arg)
-    except:
-        try:
-            float_arg = float(arg)
-            int_arg = int(arg[0: arg.find(".")])
-            if (float_arg) == int_arg:
-                return int_arg
-            else:
-                error()
-        except:
-            error()
+np.random.seed(0)
 
 
 """
