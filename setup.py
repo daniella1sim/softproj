3b7b0695211d0b479a25c44a90ba2be568aa91d3
daniella1sim <128131945+daniella1sim@users.noreply.github.com>
2024-11-16 14:42:47 +0200

    changed symnmf to symnmf module

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, Extension
 
-module = Extension("symnmf", sources=["symnmf.c", "symnmfmodule.c"])
+module = Extension("symnmfmodule", sources=["symnmf.c", "symnmfmodule.c"])
 
 setup(
-    name="symnmf",
+    name="symnmfmodule",
     version="1.0",
     description="Python interface for the symnmf C extension",
     ext_modules=[module],
